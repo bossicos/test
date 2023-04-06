10e4a3c25739125d2a1b04a7e0a102f17725549e
bossicos <129974208+bossicos@users.noreply.github.com>
2023-04-06 11:57:46 +0100

    Delete Projeto Final António directory

--- Projeto Final António/behaviour.js
+++
@@ -1,6 +0,0 @@
-let messageButton = document.querySelector("#click-button");
-messageButton.addEventListener("click", showMessage);
-function showMessage(event) {
-    let message="Obrigado por visitarem o meu site"
-    alert(message);
-}
